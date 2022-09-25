ee648c2ab30ab39b88c3478b9494e6c7d04342c1
Gayuh kautaman <gkoma444@gmail.com>
2022-09-25 10:36:52 +0700

    setup

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }: any) => {
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen ">{children}</div>
+      <Footer />
+    </>
+  );
+};
+export default Layout;
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["'Montserrat',sans-serif"],
+        sec: ["'Cormorant Garamond', serif"]
+      },
+      height: {        
+        '2s': '120vh', 
+        'bdy':'30rem',     
+      },
+      width: {        
+        '2s': '200vh',      
+      },
+      textColor: {
+        primary: "#F7F6F2",
+        secondary: "#F7F6F2",
+        danger: "#771011",
+        textSecond:"#777777",
+        numerical:'#394B39'
+      },
+      dropShadow: {
+        '3xl': '0 45px 45px rgba(0, 0, 0, 0.55)',
+      },
+      gridRow: {        
+        'span-16': 'span 7 / span 7',      
+      },
+      colors: {
+        // 500 for gradient, 600 for hover
+        primary: { DEFAULT: "#394B39" },
+        sec: { DEFAULT: "#FEB803" },
+        third: { DEFAULT: "#ECCFAF" },
+        forth: { DEFAULT: "#F0F0F0" },
+
+      },
+      screens: {
+        samsung: "370px",
+        // => @media (min-width: 640px) { ... }
+      },
+    },
+  },
+  plugins: [],
+}
